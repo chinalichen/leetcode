9e16a4f72af121bf9b49d12f3b74a4061c3b3a0b
willi <li.chen@codasheet.com>
2017-08-22 18:22:09 +0800

    EqualTreePartition finished.

--- EqualTreePartition/app.js
+++ EqualTreePartition/app.js
@@ -6,22 +6,46 @@
  * }
  */
 
-var index = 1;
-var mark = function (root) {
-  if (root == null) {
-    return;
-  }
-  root.id = index++;
-  mark(root.left);
-  mark(root.right);
-}
+// var index = 1;
+// var mark = function (root) {
+//   if (root == null) {
+//     return;
+//   }
+//   root.id = index++;
+//   mark(root.left);
+//   mark(root.right);
+// };
 
 var sumTree = function (root) {
   if (root == null) {
     return 0;
   }
   return root.val + sumTree(root.left) + sumTree(root.right);
-}
+};
+
+var partition = function (root, half) {
+
+  function dfs_ldr(node) {
+    if (node.left) {
+      if (sumTree(node.left) === half) {
+        return true;
+      } else {
+        dfs_ldr(node.left);
+      }
+    }
+
+    if (node.right) {
+      if (sumTree(node.right) === half) {
+        return true;
+      } else {
+        dfs_ldr(node.right);
+      }
+    }
+  }
+
+  const result = dfs_ldr(root);
+  return !!result;
+};
 
 
 /**
@@ -29,12 +53,24 @@ var sumTree = function (root) {
  * @return {boolean}
  */
 var checkEqualTree = function (root) {
-  mark(root);
+  const sum = sumTree(root);
+  if (sum % 2 !== 0) {
+    // console.log('False');
+    return false;
+  }
 
-  return true;
+  // mark(root);
+
+  const result = partition(root, sum / 2);
+
+  if (result) {
+    // console.log('True');
+    return true;
+  } else {
+    // console.log('False');
+    return false;
+  }
 };
-
-
 
 module.exports = {
   checkEqualTree,
--- EqualTreePartition/test.js
+++ EqualTreePartition/test.js
@@ -4,13 +4,19 @@ const assert = require('../assert');
 
 assert(0, sumTree(buildTreeFromArray([-1, 0, 1])), 'sumTree 1');
 assert(30, sumTree(buildTreeFromArray([5, 10, 10, null, null, 2, 3])), 'sumTree 2');
+assert(10, sumTree(buildTreeFromArray([5, 10, 10, null, null, 2, 3]).left), 'sumTree 3');
+assert(15, sumTree(buildTreeFromArray([5, 10, 10, null, null, 2, 3]).right), 'sumTree 4');
+assert(2, sumTree(buildTreeFromArray([5, 10, 10, null, null, 2, 3]).right.left), 'sumTree 5');
+assert(3, sumTree(buildTreeFromArray([5, 10, 10, null, null, 2, 3]).right.right), 'sumTree 6');
+assert(35, sumTree(buildTreeFromArray([1, 2, 10, 2, 20])), 'sumTree 7');
+assert(0, sumTree(buildTreeFromArray([0, -1, 1])), 'sumTree 8');
 
 
 assert(true, checkEqualTree(buildTreeFromArray([-1, 0, 1])), 'test 1 ');
 assert(true, checkEqualTree(buildTreeFromArray([5, 10, 10, null, null, 2, 3])), 'test 2 ');
-assert(false, checkEqualTree(buildTreeFromArray([-1, 0, 2])), 'test 3 ');
-assert(true, checkEqualTree(buildTreeFromArray([1, 0, 1])), 'test 4 ');
-assert(true, checkEqualTree(buildTreeFromArray([-1, 0, 1])), 'test 5 ');
-assert(true, checkEqualTree(buildTreeFromArray([-1, 0, 1])), 'test 6 ');
-assert(true, checkEqualTree(buildTreeFromArray([-1, 0, 1])), 'test 7 ');
-assert(true, checkEqualTree(buildTreeFromArray([-1, 0, 1])), 'test 8 ');
+assert(false, checkEqualTree(buildTreeFromArray([0, -1, 1])), 'test 3 ');
+assert(false, checkEqualTree(buildTreeFromArray([1, 2, 10, 2, 20])), 'test 4 ');
+// assert(true, checkEqualTree(buildTreeFromArray([-1, 0, 1])), 'test 5 ');
+// assert(true, checkEqualTree(buildTreeFromArray([-1, 0, 1])), 'test 6 ');
+// assert(true, checkEqualTree(buildTreeFromArray([-1, 0, 1])), 'test 7 ');
+// assert(true, checkEqualTree(buildTreeFromArray([-1, 0, 1])), 'test 8 ');
