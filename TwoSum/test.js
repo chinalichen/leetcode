f535aa99ae17a11ff1f14d7779779fe0c5ed7b22
willi <li.chen@codasheet.com>
2017-08-24 14:55:50 +0800

    two sum accept.

--- TwoSum/test.js
+++ TwoSum/test.js
@@ -1,4 +1,6 @@
 const twoSum = require('./app.js');
 const assert = require('../assert');
 
-assert.equal([1, 2, 3], [1, 2, 3])
+assert.deepEqual([1, 2], twoSum([3, 2, 4], 6), 'twoSum 1');
+assert.deepEqual([0, 1], twoSum([3, 3, 4], 6), 'twoSum 2');
+assert.deepEqual([0, 1], twoSum([3, 3, 3], 6), 'twoSum 3');
