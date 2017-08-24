88025f9c0595d5ca2ea9311bfef2d13d9f7b9bee
willi <li.chen@codasheet.com>
2017-08-24 14:21:51 +0800

    add TwoSum problem. add chai.assert

---
+++ TwoSum/test.js
@@ -0,0 +1,4 @@
+const twoSum = require('./app.js');
+const assert = require('../assert');
+
+assert.equal([1, 2, 3], [1, 2, 3])
--- assert.js
+++ assert.js
@@ -1,8 +1,21 @@
-function assert(expect, actual, title) {
-  if (actual !== expect) {
-    console.error(title, 'failed, expect `' + expect + '`', 'but got `' + actual + '`');
-  } else {
-    console.log(title, 'pass');
+const chaiAssert = require('./chai').assert;
+
+const assert = {};
+
+for (var k in chaiAssert) {
+  if (typeof (chaiAssert[k]) === 'function') {
+    assert[k] = getWrapped(chaiAssert);
+  }
+}
+
+function getWrapped(func) {
+  return function (expect, actual, title) {
+    try {
+      func.call(this, arguments);
+      console.log(title, 'pass');
+    } catch (e) {
+      console.error(title, 'failed, expect `' + expect + '`', 'but got `' + actual + '`');
+    }
   }
 }
 
