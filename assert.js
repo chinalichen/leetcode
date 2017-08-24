dd3e6e832603fd4dd69fc5ab2985e97720b8e20b
willi <li.chen@codasheet.com>
2017-08-24 14:55:29 +0800

    fix assert bug.

--- assert.js
+++ assert.js
@@ -4,15 +4,16 @@ const chaiAssert = require('./chai').assert;
 const assert = {};
 
 for (var k in chaiAssert) {
-  if (typeof (chaiAssert[k]) === 'function') {
-    assert[k] = getWrapped(chaiAssert);
+  const func = chaiAssert[k];
+  if (typeof (func) === 'function') {
+    assert[k] = getWrapped(func);
   }
 }
 
 function getWrapped(func) {
   return function (expect, actual, title) {
     try {
-      func.call(this, arguments);
+      func.apply(this, arguments);
       console.log(title, 'pass');
     } catch (e) {
       console.error(title, 'failed, expect `' + expect + '`', 'but got `' + actual + '`');
