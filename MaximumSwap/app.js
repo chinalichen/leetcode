52dff8fb62caf93f3b3ffabd74fbde133d0bce84
willi <li.chen@codasheet.com>
2017-09-05 11:05:41 +0800

    maximumSwap accepted:132ms. no good.

--- MaximumSwap/app.js
+++ MaximumSwap/app.js
@@ -1,17 +1,39 @@
 var maximumSwap = function (num) {
   var nums = Array.from('' + num).map(n => parseInt(n));
-  var sorted = nums.slice(0).sort().reverse();
+  var sorted = nums.map((n, i) => ({ n, w: i })).sort((n1, n2) => {
+    if (n1.n > n2.n) {
+      return -1;
+    } else if (n1.n < n2.n) {
+      return 1;
+    } else {
+      return n2.w - n1.w; // desc
+    }
+  });
 
   var i = j = 0;
-  while (nums[i] === sorted[i] && i < nums.length) {
+  while (i < nums.length && nums[i] === sorted[i].n) {
+    if (i !== sorted[i].w) {
+      j = i;
+      while (j < nums.length && nums[i] === sorted[j].n) j++;
+      if (j === nums.length) {
+        i = nums.length;
+        break;
+      } else {
+        var same = sorted.slice(i, j);
+        sorted = [].concat(sorted.slice(0, i)).concat(same.pop()).concat(same).concat(sorted.slice(j));
+      }
+    }
     i++;
   }
 
-  if(i === nums.length) {
-    return num;
-  }else {
-    
+  if (i !== nums.length) {
+    var tmp = nums[i];
+    nums[i] = sorted[i].n;
+    nums[sorted[i].w] = tmp;
   }
+
+  var str = nums.join('');
+  return parseInt(str, 10);
 };
 
 module.exports = maximumSwap;
--- MaximumSwap/test.js
+++ MaximumSwap/test.js
@@ -1,5 +1,9 @@
 const maximumSwap = require('./app.js');
 const assert = require('../assert');
 
-assert.deepEqual(7236, maximumSwap(2736), 'twoSum 1');
-assert.deepEqual(9973, twoSum(9973), 'twoSum 2');
+assert.deepEqual(7236, maximumSwap(2736), 'maximumSwap 1');
+assert.deepEqual(9973, maximumSwap(9973), 'maximumSwap 2');
+assert.deepEqual(0, maximumSwap(0), 'maximumSwap 3');
+assert.deepEqual(7762, maximumSwap(2767), 'maximumSwap 4');
+assert.deepEqual(7762, maximumSwap(7267), 'maximumSwap 5');
+assert.deepEqual(98863, maximumSwap(98368), 'maximumSwap 6');
