50f9464a917b3d5adb20879d48ddb25b926e497c
lichen_xian <li.chen@codasheet.com>
2019-12-19 23:16:34 +0800

    比较之后的K个是否有相同元素，可以通过。

--- contains-duplicate-ii/app.js
+++ contains-duplicate-ii/app.js
@@ -3,19 +3,31 @@
  * @param {number} k
  * @return {boolean}
  */
-var containsNearbyDuplicate = function (nums, k) {
-  const map = {};
-  const dup = nums.some((n, i) => {
-    if (!map[n]) {
-      map[n] = [i];
-      return;
+var hasDup = function (nums, first, k) {
+  const l = nums.length - 1;
+  const start = first + 1;
+  const end = Math.min(first + k, l);
+  const val = nums[first];
+  if (start > l) {
+    return false;
+  }
+  for (let i = Math.min(first + 1, l); i <= end; i++) {
+    if (nums[i] !== val) {
+      continue;
     }
-    if (map[n].some(n => Math.abs(n - i) <= k)) {
+    if (Math.abs(i - first) <= k) {
       return true;
     }
-    map[n].push(i);
-  });
-  return dup;
+  }
+  return false;
+}
+var containsNearbyDuplicate = function (nums, k) {
+  for (let i = 0; i < nums.length; i++) {
+    if (hasDup(nums, i, k)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 module.exports = containsNearbyDuplicate;
