ee28ca342d552c1f19748053f95c69b3d7afa4f9
lichen_xian <li.chen@codasheet.com>
2019-12-28 15:13:41 +0800

    头尾指针。

--- squares-of-a-sorted-array/app.js
+++ squares-of-a-sorted-array/app.js
@@ -3,7 +3,21 @@
  * @return {number[]}
  */
 var sortedSquares = function (A) {
-  return A.map(v=>v*v).sort((a,b)=>a-b);
+  let min = 0;
+  let max = A.length - 1;
+  const result = [];
+  while (max >= min) {
+    const maxAbs = Math.abs(A[max]);
+    const minAbs = Math.abs(A[min]);
+    if (minAbs > maxAbs) {
+      min++;
+      result.push(minAbs * minAbs);
+    } else {
+      max--;
+      result.push(maxAbs * maxAbs);
+    }
+  }
+  return result.reverse();
 };
 
 module.exports = sortedSquares;
