7560a856f534f18cd99dfc7882bb97be78ecdf6a
lichen_xian <li.chen@codasheet.com>
2019-12-28 23:48:50 +0800

    Bucket sort

--- height-checker/app.js
+++ height-checker/app.js
@@ -3,7 +3,26 @@
  * @return {number}
  */
 var heightChecker = function (heights) {
-  const sorted = heights.slice(0).sort((a, b) => a - b);
-  return heights.reduce((count, curr, index) => curr === sorted[index] ? count : count + 1, 0);
+  const buckets = [];
+  for (let i = 0; i <= 100; i++) {
+    buckets[i] = 0;
+  }
+  for (let i = 0; i < heights.length; i++) {
+    buckets[heights[i]] += 1;
+  }
+  let count = 0;
+  let pos = 0;
+  for (let i = 0; i <= 100; i++) {
+    if (buckets[i] === 0) {
+      continue;
+    }
+    for (let j = 0; j < buckets[i]; j++) {
+      if (heights[pos] !== i) {
+        count++;
+      }
+      pos++;
+    }
+  }
+  return count;
 };
 module.exports = heightChecker;
