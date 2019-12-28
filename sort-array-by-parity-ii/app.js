638dc6fbe1b346dababa0f91835f36528fadb501
lichen_xian <li.chen@codasheet.com>
2019-12-28 15:03:43 +0800

    找到余下数组中不符合的odd和even交换即可。

--- sort-array-by-parity-ii/app.js
+++ sort-array-by-parity-ii/app.js
@@ -5,18 +5,21 @@
 var sortArrayByParityII = function (A) {
   let odd = 1;
   let even = 0;
-  const result = [];
-  for (let i = 0; i < A.length; i++) {
-    const v = A[i];
-    if (v % 2 === 0) {
-      result[even] = v;
-      even += 2;
-    } else {
-      result[odd] = v;
+  let len = A.length;
+  for (let i = 0; i < len; i++) {
+    while (A[odd] % 2 === 1) {
       odd += 2;
     }
+    while (A[even] % 2 === 0) {
+      even += 2;
+    }
+    if (odd < len && even < len) {
+      const v = A[odd];
+      A[odd] = A[even];
+      A[even] = v;
+    }
   }
-  return result;
+  return A;
 };
 
 module.exports = sortArrayByParityII;
