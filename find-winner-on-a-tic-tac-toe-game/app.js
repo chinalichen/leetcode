647ab6055081ba70db6f8ed785fba5dbe1b0d325
lichen_xian <li.chen@codasheet.com>
2020-01-01 01:10:52 +0800

    位运算，在js中快不了。

--- find-winner-on-a-tic-tac-toe-game/app.js
+++ find-winner-on-a-tic-tac-toe-game/app.js
@@ -5,28 +5,28 @@
 const sameValue = (a, b, c) => (a === b) && (b === c) && (a != null);
 
 var tictactoe = function (moves) {
-  const map = [[], [], []];
-  moves.forEach(([r, c], i) => {
-    const v = i % 2 === 0 ? 'A' : 'B';
-    map[r][c] = v;
-  });
-  for (let i = 0; i < 3; i++) {
-    const row = map[i];
-    if (sameValue(row[0], row[1], row[2])) {
-      return row[0];
+  let a = 0;
+  let b = 0;
+  for (let i = 0; i < moves.length; i++) {
+    const [r, c] = moves[i];
+    if (i % 2 === 0) {
+      a |= 1 << (r * 3 + c);
+    } else {
+      b |= 1 << (r * 3 + c);
     }
   }
-  for (let i = 0; i < 3; i++) {
-    if (sameValue(map[0][i], map[1][i], map[2][i])) {
-      return map[0][i];
+
+  const wins = [7, 56, 448, 73, 146, 292, 273, 84];
+  for (let i = 0; i < wins.length; i++) {
+    const v = wins[i];
+    if ((v & a) === v) {
+      return 'A';
+    }
+    if ((v & b) === v) {
+      return 'B';
     }
   }
-  if (sameValue(map[0][0], map[1][1], map[2][2])) {
-    return map[0][0];
-  }
-  if (sameValue(map[0][2], map[1][1], map[2][0])) {
-    return map[0][2];
-  }
+
   return moves.length >= 9 ? "Draw" : "Pending";
 };
 module.exports = tictactoe;
