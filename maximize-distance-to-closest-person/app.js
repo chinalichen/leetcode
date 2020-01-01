9519be6236a696d5da73bf227a6a36e52bad8dd0
lichen_xian <li.chen@codasheet.com>
2020-01-01 22:27:07 +0800

    left[i] = left[i-1]+1

--- maximize-distance-to-closest-person/app.js
+++ maximize-distance-to-closest-person/app.js
@@ -2,47 +2,34 @@
  * @param {number[]} seats
  * @return {number}
  */
-const minDist = (arr, find) => {
-  if (arr.length === 1) {
-    return Math.abs(find - arr[0]);
-  }
-  let head = 0;
-  let tail = arr.length - 1;
-  let delta = 9999999;
-  while (head <= tail) {
-    const pos = Math.floor((head + tail) / 2);
-    const v = arr[pos];
-    if (Math.abs(find - v) > delta) {
-      return delta;
-    }
-    if (v > find) {
-      tail = pos - 1;
-    } else if (v < find) {
-      head = pos + 1;
-    } else {
-      return 0;
-    }
-  }
-  const v1 = arr[head] == null ? 99999999 : arr[head];
-  const v2 = arr[tail] == null ? 99999999 : arr[tail];
-  return Math.min(Math.abs(find - v1), Math.abs(find - v2));
-};
 
 var maxDistToClosest = function (seats) {
-  const map = [];
+  const left = [];
+  const right = [];
   for (let i = 0; i < seats.length; i++) {
-    if (seats[i] === 1) {
-      map.push(i);
+    if (seats[i] === 0) {
+      if (i === 0) {
+        left[i] = 99999999;
+        continue;
+      }
+      left[i] = left[i - 1] == null ? 1 : left[i - 1] + 1;
+    }
+  }
+  for (let i = seats.length - 1; i >= 0; i--) {
+    if (seats[i] === 0) {
+      if (i === seats.length - 1) {
+        right[i] = 99999999;
+        continue;
+      }
+      right[i] = right[i + 1] == null ? 1 : right[i + 1] + 1;
     }
   }
 
   let max = -1;
   for (let i = 0; i < seats.length; i++) {
-    if (seats[i] === 0) {
-      const min = minDist(map, i);
-      if (max < min) {
-        max = min;
-      }
+    const d = Math.min(left[i], right[i]);
+    if (max < d) {
+      max = d;
     }
   }
   return max;
