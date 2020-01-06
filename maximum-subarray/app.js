a6d15e4a49d62ec44dc31d3b1e84d2bd3257f50f
chinalichen <chinalichen@126.com>
2020-01-06 13:57:21 +0800

    finally

--- maximum-subarray/app.js
+++ maximum-subarray/app.js
@@ -2,20 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-
-function sub(array) {
-  const num = array[array.length - 1];
-  if (array.length === 1) {
-    return { max: num, tailSum: Math.max(num, 0) };
-  }
-  const res = sub(array.slice(0, -1));
-  const sum = res.tailSum + num;
-  const max = Math.max(res.max, sum);
-  return { max, tailSum: Math.max(0, sum) };
-}
-
 var maxSubArray = function (nums) {
-  return sub(nums).max;
+  let max = -2147483647;
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (max < sum) {
+      max = sum;
+    }
+    if (sum < 0) {
+      sum = 0;
+    }
+  }
+  return max;
 };
 
 module.exports = maxSubArray;
