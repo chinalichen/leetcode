60f4f1894eada859fa3a07de2735623d56b42f34
chinalichen <chinalichen@126.com>
2020-01-09 14:07:17 +0800

    xor 大法好

--- find-the-difference/app.js
+++ find-the-difference/app.js
@@ -4,22 +4,15 @@
  * @return {character}
  */
 var findTheDifference = function (s, t) {
-  const sm = {};
+  let code = 0;
+
   for (let i = 0; i < s.length; i++) {
-    const c = s[i];
-    sm[c] = sm[c] == null ? 1 : sm[c] + 1;
+    code ^= s.charCodeAt(i);
   }
-  const tm = {};
   for (let i = 0; i < t.length; i++) {
-    const c = t[i];
-    tm[c] = tm[c] == null ? 1 : tm[c] + 1;
+    code ^= t.charCodeAt(i);
   }
-  for (let c in tm) {
-    if (tm[c] > (sm[c] || 0)) {
-      return c;
-    }
-  }
-  throw Error("lalala");
+  return String.fromCharCode(code);
 };
 
 module.exports = findTheDifference;
