257556ed667e06cc22875224b3d9e06a94f77f8c
chinalichen <chinalichen@126.com>
2020-01-09 14:01:23 +0800

    use map.

--- find-the-difference/app.js
+++ find-the-difference/app.js
@@ -4,23 +4,20 @@
  * @return {character}
  */
 var findTheDifference = function (s, t) {
-  if (s === t) {
-    return null;
+  const sm = {};
+  for (let i = 0; i < s.length; i++) {
+    const c = s[i];
+    sm[c] = sm[c] == null ? 1 : sm[c] + 1;
   }
-  if (s.length === 0) {
-    return t;
+  const tm = {};
+  for (let i = 0; i < t.length; i++) {
+    const c = t[i];
+    tm[c] = tm[c] == null ? 1 : tm[c] + 1;
   }
-  if (s.length === 1) {
-    return t[0] === s ? t[1] : t[0];
-  }
-  const pos = Math.floor(s.length / 2);
-  const s1 = findTheDifference(s.slice(0, pos), t.slice(0, pos));
-  if (s1 != null) {
-    return s1;
-  }
-  const s2 = findTheDifference(s.slice(pos), t.slice(pos));
-  if (s2 != null) {
-    return s2;
+  for (let c in tm) {
+    if (tm[c] > (sm[c] || 0)) {
+      return c;
+    }
   }
   throw Error("lalala");
 };
