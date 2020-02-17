f4abef59be4435de0c6ee2502d27a9050c9bbb84
chinalichen <chinalichen@126.com>
2020-02-17 21:30:28 +0800

    add `npm run generate test` command.

--- set-mismatch/app.js
+++ set-mismatch/app.js
@@ -4,17 +4,13 @@
  */
 var findErrorNums = function (nums) {
     const n = nums.length;
-    const dupMap = {};
-    let dup = 0;
+    let withDup = 0;
     for (let i = 0; i < n; i++) {
-        const v = nums[i];
-        if (dupMap[v]) {
-            dup = v;
-            break;
-        }
-        dupMap[v] = true;
+        withDup ^= i;
     }
+    const withoutDup = nums.reduce((s, v) => s ^ v, withDup);
 
+    const dup = withDup - withoutDup;
     const correctSum = n * (n + 1) / 2;
     const sum = nums.reduce((s, v) => s + v);
     const delta = correctSum - sum;
